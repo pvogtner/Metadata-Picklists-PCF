701486a9934c21bb7280da20a4c39b99ed34a0fc
Patrick Vogtner <patrick@dynamicconsultantsgroup.com>
2025-06-17 09:17:36 -0500

    Added advanced filtering as input parameter for field types

--- MetadataPicklistAttribute/MetadataPicklistAttribute/useAttributeMetadata.ts
+++ MetadataPicklistAttribute/MetadataPicklistAttribute/useAttributeMetadata.ts
@@ -4,12 +4,46 @@ import { IAttributeMetadata, IAttributeMetadataResponse, IOption } from "./Inter
 
 export const useAttributeMetadata = (Context: ComponentFramework.Context<IInputs>, tableName: string) => {
     const [options, setOptions] = React.useState<IOption[]>([]);
-    console.log("GETTING METADATA: ", tableName);
+
     React.useEffect(() => {
         const getMetadata: (tableName: string) => Promise<IAttributeMetadataResponse> = async (tableName: string) => {
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
             const baseUrl: string = (Context as any).page.getClientUrl();
-            const url = `${baseUrl}/api/data/v9.2/EntityDefinitions(LogicalName='${tableName}')/Attributes?$select=AttributeType,LogicalName,DisplayName&$filter=AttributeOf eq null`;
+            const customFilter: string | null | undefined = Context.parameters.customFilter?.raw;
+            let filter = "AttributeOf eq null";
+            switch (customFilter) {
+                case "Boolean":
+                    filter += " and AttributeType eq 'Boolean'";
+                    break;
+                case "DateTime":
+                    filter += " and AttributeType eq 'DateTime'";
+                    break;
+                case "Decimal":
+                    filter += " and AttributeType eq 'Decimal'";
+                    break;
+                case "Double":
+                    filter += " and AttributeType eq 'Double'";
+                    break;
+                case "Integer":
+                    filter += " and AttributeType eq 'Integer'";
+                    break;
+                case "String":
+                    filter += " and AttributeType eq 'String'";
+                    break;
+                case "Picklist":
+                    filter += " and (AttributeType eq 'Picklist' or AttributeType eq 'State' or AttributeType eq 'Status')";
+                    break;
+                case "Lookup":
+                    filter += " and (AttributeType eq 'Lookup' or AttributeType eq 'Customer' or AttributeType eq 'Owner' or AttributeType eq 'Uniqueidentifier')";
+                    break;
+                case "Number":
+                    filter += " and (AttributeType eq 'Decimal' or AttributeType eq 'Double' or AttributeType eq 'Integer' or AttributeType eq Money)";
+                    break;
+                default:
+                    break;
+            };
+
+            const url = `${baseUrl}/api/data/v9.2/EntityDefinitions(LogicalName='${tableName}')/Attributes?$select=AttributeType,LogicalName,DisplayName&$filter=${filter}`;
             const response = await fetch(url, {
                 method: "GET",
                 headers: {
